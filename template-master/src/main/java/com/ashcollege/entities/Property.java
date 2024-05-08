217c2ddc89fe127712248f738a64feeff0cd0f2e
AnaelAhimeir <151847176+AnaelAhimeir@users.noreply.github.com>
2024-05-08 22:25:10 +0300

    Add files via upload

---
+++ template-master/src/main/java/com/ashcollege/entities/Property.java
@@ -0,0 +1,40 @@
+package com.ashcollege.entities;
+
+public class Property {
+    private int id;
+    private String name;
+    private String address;
+    private int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
